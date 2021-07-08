5016a4322f0170c3fe914f0ead0448a9451a1b90
Aigerim77 <77402879+Aigerim77@users.noreply.github.com>
2021-07-08 13:52:36 +0300

    added feedback admin

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
-# Create your tests here.
+
+class SeleniumTest(TestCase):
+    def test_one(self):
+        driver =webdriver.Chrome()
+        driver.get('http://localhost:8000/')
+        driver.find_element_by_link_text('Создать место').click()
+        sleep(5)
+        #elem.send_keys('pycon')
+        #elem.send_keys(Keys.RETURN)
+        #assert "No results found." not in driver.page_source
+        driver.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
